e8c4c069e1aeda555f5fdb8b1b3b3999b1354659
Vincent Leclerc <vleclerc@inow.fr>
2025-10-21 14:25:55 +0200

    HttpRequestMethodNotSupportedException

--- spring-mvc/src/main/java/com/formation/api/exceptions/ApiExceptionHandler.java
+++ spring-mvc/src/main/java/com/formation/api/exceptions/ApiExceptionHandler.java
@@ -4,11 +4,12 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -21,5 +22,14 @@ public class ApiExceptionHandler {
         body.put("message", "Validation échouée");
         body.put("errors", errors);
         return ResponseEntity.badRequest().body(body);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(HttpRequestMethodNotSupportedException ex) {
+
+        Map<String, String> body = new LinkedHashMap<>();
+        body.put("message", "HTTP ERROR 405 Method is not supported.");
+        body.put("errors", "BOULET !!!!");
+        return ResponseEntity.status(405).body(body);
     }
 }
